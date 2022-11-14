450ec10d118706a8f41783b6caa3a083cada1927
Qitong Hu <106654749+QitongHu2000@users.noreply.github.com>
2022-11-14 15:41:07 +0800

    Add some files
    1) Add BlurAge into blur.java. 2) Add MaskLcation into mask.java. 3) Add the second generation of BlurTime in order to operate examples like 2022年5月 and 2022年.

--- src/Algorithm/Blur.java
+++ src/Algorithm/Blur.java
@@ -1,16 +1,19 @@
 package Algorithm;
 
 import java.security.NoSuchAlgorithmException;
+
 import java.util.Vector;
 public class Blur{
 	public class BlurTime extends Blur{
-		private static int[][] splits = {{0,4,6,8},{0,4,6,7},{0,4,5,7},{0,4,5,6},
-										{0,2,4,6},{0,2,3,5},{0,2,4,5},{0,2,3,4}};
+		private static int[][] splits = {{0,4,6,8},{0,4,6,7},{0,4,5,7},{0,4,6,6},
+										{0,4,5,6},{0,2,4,6},{0,4,5,5},{0,2,3,5},
+										{0,2,4,5},{0,4,4,4},{0,2,4,4},{0,2,3,4},
+										{0,2,3,3},{0,2,2,2}};
 		private static int[] daysLeapYear = {31,29,31,30,31,30,31,31,30,31,30,31};
 		private static int[] daysNonLeapYear = {31,28,31,30,31,30,31,31,30,31,30,31};
 		private static String[] linkWord = {"年","月","日"};
 		
-		private static boolean checkStandardTime (String[] ymdString) {
+		private static boolean checkStandardTime(String[] ymdString) {
 			boolean isStandard = true;
 			
 			int year = Integer.parseInt(ymdString[0]);
@@ -20,9 +23,12 @@ public class Blur{
 				if (year<1949 || year>2048) isStandard=false;
 			}
 			
+			if(ymdString[1] == "") return isStandard;
+			
 			int month = Integer.parseInt(ymdString[1]);
 			if(month == 0 || month > 12) isStandard=false;
 			
+			if(ymdString[2] == "") return isStandard;
 			int day = Integer.parseInt(ymdString[2]);
 			if (isStandard) {
 				year = (year >= 1900) ? year : year + 1900;
@@ -92,33 +98,8 @@ public class Blur{
 			return StandardTimeSeries;
 		}
 	
-		private static Vector<String[]> getStandardTimes(String input) throws NoSuchAlgorithmException {
-			Vector<String[]> StandardTimeSeriesVector = new Vector<String[]>();
-			
-			String Time = input.replaceAll("([^0-9])", " ");
-			Time = Time.replaceAll("\\s{1,}", " ");
-			Vector<Integer> spaceLocations = getPositionofSpace(Time);
-			Time = Time.replaceAll("\\s","");
-			
-			boolean isStandard = false;
-			for (int k=0; k<splits.length; k++) {
-				int[] split = splits[k];
-				if(split[3] > Time.length()) continue;
-				if(!comparePositionofSpace(spaceLocations, split)) continue;
-				String[] ymdString = {Time.substring(split[0],split[1]),
-									Time.substring(split[1],split[2]),
-									Time.substring(split[2],split[3])};
-				if (checkStandardTime(ymdString)) {
-					StandardTimeSeriesVector.add(ymdString);
-					isStandard = true;
-				}
-			}
-			if (!isStandard) throw new NoSuchAlgorithmException();
-			return StandardTimeSeriesVector;
-		}
-	
 		public static String blur(String input) throws NoSuchAlgorithmException {
-			String StandardTime=null;
+			String StandardTime = "";
 			try {
 				StandardTime = blur(input,"year");
 			} catch (NoSuchAlgorithmException e) {
@@ -128,7 +109,7 @@ public class Blur{
 		}
 		
 		public static String blur(String input,String level) throws NoSuchAlgorithmException {
-			String StandardTime = null;
+			String StandardTime = "";
 			try {
 				int count=0;
 				switch (level) {
@@ -143,6 +124,7 @@ public class Blur{
 				StringBuffer StandardTimeBuffer = new StringBuffer();
 				for(int k = 0; k < count; k++) {
 					String TimeElement = StandardTimeSeries[k];
+					if(TimeElement == "" || TimeElement == null) continue;
 					int Time = Integer.parseInt(TimeElement);
 					if(k==0) {
 						Time = (Time >= 1900) ? Time : Time + 1900;
@@ -160,61 +142,31 @@ public class Blur{
 			}
 			return StandardTime;
 		}
-		
-		public static Vector<String> blurs(String input) throws NoSuchAlgorithmException {
-			Vector<String> StandardTime=null;
-			try {
-				StandardTime = blurs(input,"year");
-			} catch (NoSuchAlgorithmException e) {
-				e.printStackTrace();
-			}
-			return StandardTime;
-		}
-		
-		public static Vector<String> blurs(String input,String level) throws NoSuchAlgorithmException {
-			String StandardTime = null;
-			Vector<String> StandardTimes = new Vector<String>();
-			try {
-				int count=0;
-				switch (level) {
-					case "year": count=1; break;
-					case "month": count=2; break;
-					case "day": count=3; break;
-					default: break;
-				}
-				if(count==0) throw new IllegalArgumentException();
-				
-				Vector<String[]> StandardTimeSeriesVector = getStandardTimes(input);
-				for(int i = 0; i < StandardTimeSeriesVector.size(); i++) {
-					String[] StandardTimeSeries = StandardTimeSeriesVector.get(i);
-					StringBuffer StandardTimeBuffer = new StringBuffer();
-					for(int k = 0; k < count; k++) {
-						String TimeElement = StandardTimeSeries[k];
-						int Time = Integer.parseInt(TimeElement);
-						if(k==0) {
-							Time = (Time >= 1900) ? Time : Time + 1900;
-							Time = (Time >= 1949) ? Time : Time + 100;
-						}
-						StandardTimeBuffer.append(Time);
-						StandardTimeBuffer.append(linkWord[k]);
-					}
-					StandardTime = StandardTimeBuffer.toString();
-					StandardTimes.add(StandardTime);
-				}
-			} catch (NoSuchAlgorithmException e) {
-				e.printStackTrace();
-			} catch (IllegalArgumentException e) {
-				System.out.println("The value of level should be chosen in year/month/day.");
-				e.printStackTrace();
-			}
-			return StandardTimes;
-		}
 	}
-
-	//TODO: blur age
-	public class BlurAge extends Blur {
-		public static String run(String age) {
-			return age;
+	
+	public static class BlurAge extends Blur {
+		private static int getStandardAge(String input) {
+			String Age = input.replaceAll("[^0-9]","");
+			int standardAgeValue = Integer.parseInt(Age);
+			return standardAgeValue;
+		}
+		
+		public static String blur(String input) throws NoSuchAlgorithmException{
+			String StandardAge = null;
+			try {
+				StandardAge = blur(input,"10");
+			} catch (NoSuchAlgorithmException e) {
+				e.printStackTrace();
+			}
+			return StandardAge;
+		}
+		
+		public static String blur(String input, String level) throws NoSuchAlgorithmException {
+			int count = Integer.parseInt(level);
+			int standardAgeValue = getStandardAge(input);
+			int period = standardAgeValue / count;
+			String standardAge = String.format("%d岁-%d岁",period*count,(period+1)*count);
+			return standardAge;
 		}
 	}
 }
--- src/Algorithm/Mask.java
+++ src/Algorithm/Mask.java
@@ -1,11 +1,152 @@
 package Algorithm;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class Mask {
-
-  public class MaskAddr extends Mask {
-
-    public static String run(String addr) {
-      return addr;
-    }
-  }
+	public class MaskLocation {
+		@SuppressWarnings("rawtypes")
+		static HashMap<String,HashMap> base = null;
+		
+		@SuppressWarnings({ "rawtypes", "unchecked" })
+		public static void setLocationsName() throws NoSuchAlgorithmException{
+			final File file = new File("data/location.csv");
+			file.setReadable(true);
+		    BufferedReader reader;
+	        HashMap<String, HashMap> china = new HashMap<String, HashMap>();
+			try {
+				reader = new BufferedReader(new FileReader(file));
+		        String record = null;
+		        
+		        HashMap<String, HashMap> provience = null;
+		        HashMap<String, HashMap> city = null;
+		        String provienceName = null;
+		        String cityName = null;
+		        String countryName = null;
+		        while ((record = reader.readLine()) != null) {
+		            String[] keywords = record.split("\\t");
+		            int level = Integer.parseInt(keywords[0]);
+		            String name = keywords[3];
+		            String mergeName = keywords[4];
+		            String[] simpliedNameLinkWord = mergeName.split(",");
+		            String simpliedName = simpliedNameLinkWord[simpliedNameLinkWord.length-1];
+		            String linkWord = name.replace(simpliedName,"");
+		            if(linkWord == null) linkWord = "";
+		            if(name.equals("市辖区") || name.equals( "直辖区") || name.equals("直辖县")) continue;
+		            switch(level) {
+			            case 0:
+			            	provienceName = simpliedName+","+linkWord;
+			            	china.put(provienceName, new HashMap<String, HashMap>());
+			            	provience = china.get(provienceName);
+			            	break;
+			            case 1:
+			            	cityName = simpliedName+","+linkWord;
+			            	provience.put(cityName, new HashMap<String, HashMap>());
+			            	city = provience.get(cityName);
+			            	break;
+			            case 2:
+			            	countryName = simpliedName+","+linkWord;
+			            	city.put(countryName, null);
+			            	break;
+			            case 3:
+			            	cityName = simpliedName+","+linkWord;
+			            	provience.put(cityName, null);
+			            	break;
+		            }
+		        }
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}			
+			base = china;
+		}
+		
+		@SuppressWarnings("rawtypes")
+		private static String checkStandardLocation (String location, HashMap<String, HashMap> map) throws NoSuchAlgorithmException {
+			Iterator<Map.Entry<String, HashMap>> iterator = map.entrySet().iterator();
+			String selectedLocation = null;
+			int minimumLoc = location.length()+1;
+			while (iterator.hasNext()) {
+				Map.Entry<String, HashMap> entry = iterator.next();
+				String key = entry.getKey();
+				String subLocation = key.split(",")[0];
+				int firstLoc = location.indexOf(subLocation);
+				if(firstLoc != -1 && minimumLoc > firstLoc) {
+					minimumLoc = firstLoc;
+					selectedLocation = key;
+				}
+			}
+			if(selectedLocation == null) throw new NoSuchAlgorithmException();
+			else return selectedLocation;
+		}
+		
+		@SuppressWarnings({ "rawtypes", "unchecked" })
+		private static String[] getStandardLocation (String input, HashMap<String, HashMap> fathermap) throws NoSuchAlgorithmException {
+			String[] StandardLocationSeries = new String[3];
+			String Location = input.replaceAll("([^\\u4e00-\\u9fa5])", "");
+			Location.replaceAll("中国|天朝|中华人民共和国|大陆", "");
+			HashMap<String, HashMap> map = fathermap;
+			for (int k = 0; k < 3 && map != null; k++) {
+				try {
+					String subLocationLinkWord = checkStandardLocation(Location, map);
+					String[] subLocationLinkWordList = subLocationLinkWord.split(",");
+					String subLocation = subLocationLinkWordList[0];
+					String subLinkWord = (subLocationLinkWordList.length == 2) ? subLocationLinkWordList[1] : "";
+					Location = Location.replaceFirst(subLocation, "");
+					if (k<2) map = map.get(subLocationLinkWord);
+					StandardLocationSeries[k] = subLocation + subLinkWord;
+				} catch (NoSuchAlgorithmException e) {break;}
+			} 
+			return StandardLocationSeries;
+		}
+		
+		public static String mask(String input) throws NoSuchAlgorithmException {
+			String standardQuickLocation = "";
+			try {
+				standardQuickLocation = mask(input, "provience");
+			} catch (NoSuchAlgorithmException e) {
+				e.printStackTrace();
+			}
+			return standardQuickLocation;
+		}
+		
+		public static String mask(String input, String level) throws NoSuchAlgorithmException {
+			String StandardQuickLocation = "";
+			try {
+				int count=0;
+				switch (level) {
+					case "provience": count = 1; break;
+					case "city": count = 2; break;
+					case "country": count = 3; break;
+					default: break;
+				}
+				if(count==0) throw new IllegalArgumentException();
+				
+				String[] StandardLocationSeries = getStandardLocation(input, base);
+				StringBuffer StandardLocationBuffer = new StringBuffer();
+				for(int k = 0; k < count; k++) {
+					String LocationElement = StandardLocationSeries[k];
+					if (LocationElement == null) continue;
+					else StandardLocationBuffer.append(LocationElement);
+				}
+				StandardQuickLocation = StandardLocationBuffer.toString();
+				StandardQuickLocation.replace("高开区","高新技术产业开发区");
+				StandardQuickLocation.replace("经开区", "经济开发区");
+			} catch (NoSuchAlgorithmException e) {
+				e.printStackTrace();
+			} catch (IllegalArgumentException e) {
+				System.out.println("The value of level should be chosen in provience/city/country.");
+				e.printStackTrace();
+			}
+			return StandardQuickLocation;
+		}
+	}
 }
